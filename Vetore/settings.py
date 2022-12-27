b276b0473e2e8aa4c7789b6f9cf2c3083aa2a53e
vettorepromotora <suportevettore@outlook.com>
2022-12-27 14:17:58 -0300

    Atualização settings

--- Vetore/settings.py
+++ Vetore/settings.py
@@ -149,10 +149,12 @@ LANGUAGES = DJANGO_LANGUAGES
 # Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
